d4da9c24145e087c415138f2114e07ede352c641
Michael <admin@fillthegaplearning.co.uk>
2021-01-18 22:59:10 +0000

    Initial commit.

---
+++ src/main/java/com/codeclan/example/pirateservice_d1_starter/controllers/ShipController.java
@@ -0,0 +1,4 @@
+package com.codeclan.example.pirateservice_d1_starter.controllers;
+
+public class ShipController {
+}
---
+++ src/main/java/com/codeclan/example/pirateservice_d1_starter/models/Ship.java
@@ -0,0 +1,38 @@
+package com.codeclan.example.pirateservice_d1_starter.models;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+
+@Entity
+@Table(name = "ships")
+
+
+public class Ship {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @OneToMany(mappedBy = "ship")
+    private List<Pirate> pirates;
+
+    public Ship(){
+
+    }
+
+    public Ship(String name){
+        this.name = name;
+        this.pirates = new ArrayList<Pirate>();
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
